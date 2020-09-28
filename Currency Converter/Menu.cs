c0bc93f9f0734a07efc7bc0a2eb4ef3e7706f25b
Don <64542224+penguin1923@users.noreply.github.com>
2020-09-28 12:41:29 -0400

    start of program only pulls the JSON down

---
+++ Currency Converter/Menu.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Currency_Converter
+{
+    public class Menu
+    {
+        public void MenuStart() 
+        {
+            Console.WriteLine("Welcome to the currency converter program");
+            Console.ReadLine();
+            ListCurrency();
+        }
+        public void ListCurrency() 
+        {
+            //
+            var client = new RestClient("https://free.currconv.com/api/v7/currencies?apiKey=f2196ac580f903faf2c4");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Cookie", "__cfduid=d5ff7c2076506b64a724a9761178bb3e51601307747");
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+    }
+}
---
+++ Currency Converter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Currency_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.MenuStart();
+        }
+    }
+}
